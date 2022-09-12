9b9f6e7ea04fda1443965378a6ff80f59cc0a875
nyxfor13days <nyxfor13days@gmail.com>
2022-09-12 20:07:37 +0530

    configured tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './main.css';
 
 library.add(far, fas, fab);
 
