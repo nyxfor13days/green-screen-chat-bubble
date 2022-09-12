7a7a27b508774f478a99cd7bd882f4842741e4bd
nyxfor13days <nyxfor13days@gmail.com>
2022-09-12 21:49:12 +0530

    created an input

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,7 +5,7 @@ const Chat = () => {
 	const [messages, setMessages] = React.useState<string[]>(['Hello']);
 
 	return (
-		<div className='chat p-4 bg-green-600 flex flex-col gap-4 items-end justify-end'>
+		<div className='chat p-4 flex flex-col gap-4 items-end justify-end'>
 			{messages.map((message, index) => (
 				<Message key={index} message={message} />
 			))}
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const Input = () => {
-	return <div className='h-16 bg-green-400'>Input</div>;
+	const [message, setMessage] = React.useState('');
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log(message);
+		setMessage('');
+	};
+
+	return (
+		<div className='h-16'>
+			<form
+				onSubmit={handleSubmit}
+				className='w-full h-full flex justify-center items-center'>
+				<input
+					type='text'
+					placeholder='Enter your message'
+					aria-placeholder='Enter your message'
+					value={message}
+					onChange={(event) => setMessage(event.target.value)}
+				/>
+				<button className='hidden' type='submit'>
+					Send
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Input;
